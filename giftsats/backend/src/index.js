030299f3efef34de9a33340e4b10df30dfc4ba28
angpaohub <pinarat09@outlook.com>
2026-05-20 14:20:21 +0700

    index.js

--- giftsats/backend/src/index.js
+++ giftsats/backend/src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'; 
 import multer from 'multer';
 import path from 'path';
 import { randomUUID } from 'crypto';
